f1f434898456044a99c3eb9f4a853c05252d33fe
Siarhei Sharykhin <siarhei.sharykhin@gmail.com>
2015-08-24 08:45:57 +0300

    parsing support this

--- test/parse_test.js
+++ test/parse_test.js
@@ -152,6 +152,26 @@ describe("parse", function() {
 		});
 	}); // end
 
+	it('looks up an attribute from the scope', function() {
+		var fn = parse('aKey');
+		expect(fn({
+			aKey: 42
+		})).toBe(42);
+		expect(fn({})).toBeUndefined();
+	}); // end
+
+	it("returns undefined when looking up attribute from undefined", function() {
+		var fn = parse('aKey');
+		expect(fn()).toBeUndefined();
+	}); // end
+
+	it("will parse this", function() {
+		var fn = parse('this');
+		var scope = {};
+		expect(fn(scope)).toBe(scope);
+		expect(fn()).toBeUndefined();
+	}); // end
+
 
 
 }); // end describe parse
